91876e55a6d1243b656292db73783733534514d8
Sean Lewis <slewis@instructure.com>
2015-06-25 19:28:30 -0600

    lint time_helper.rb

--- lib/soundcloud2000/time_helper.rb
+++ lib/soundcloud2000/time_helper.rb
@@ -1,6 +1,8 @@
 require 'time'
 
 module Soundcloud2000
+  # handles proper time display
+  # TODO: make this better looking or find an alternative
   module TimeHelper
     HOUR    = 1000 * 60 * 60
     MINUTE  = 1000 * 60
@@ -15,8 +17,7 @@ module Soundcloud2000
 
       parts.shift if parts.first.zero?
 
-      [ parts.first, *parts[1..-1].map { |part| '%02d' % part } ].join('.')
+      [parts.first, *parts[1..-1].map { |part| "%02d#{part}" }].join('.')
     end
-
   end
 end
