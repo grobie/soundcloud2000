fe4b7cd30b4d34987c87a7e6a225ac7974c4d560
Dean Hudson <dean@soundcloud.com>
2015-10-05 21:18:24 +0200

    Require the user supply a client id

--- lib/soundcloud2000.rb
+++ lib/soundcloud2000.rb
@@ -2,9 +2,13 @@ require_relative 'soundcloud2000/client'
 require_relative 'soundcloud2000/application'
 
 module Soundcloud2000
-  CLIENT_ID = '29f8e018e1272c27bff7d510a10da2a8'
 
   def self.start
+    unless CLIENT_ID = ENV['SC_CLIENT_ID']
+      puts "You need to set SC_CLIENT_ID to a valid client ID"
+      exit 1
+    end
+
     client = Client.new(CLIENT_ID)
     application = Application.new(client)
 
